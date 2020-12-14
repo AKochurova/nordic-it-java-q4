67eea4a236370207105f6a9ed2fefd27332ce158
AKochurova <kochurovaalena@mail.ru>
2020-12-14 22:29:29 +0700

    'исправленоэ'

--- Zad1/src/sql/demo/Main.java
+++ Zad1/src/sql/demo/Main.java
@@ -7,54 +7,44 @@ import java.util.Scanner;
 public class Main {
 
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Введите ширину рамки");
-        int a = scanner.nextInt();
+        int na = scanner.nextInt();
         System.out.println("Введите длину рамки");
-        int b = scanner.nextInt();
+        int nb = scanner.nextInt();
         System.out.println("Введите строку");
         String word = reader.readLine();
 
         //Если длина строки больше ширины рамки выводит "Ошибка"
-        if (b < word.length()) {
+        if (nb < word.length() || na <= 2) {
             System.out.println("Ошибка");
-        }
-        else {
+        } else {
             //Выводит а строк, состоящих из "*"
-            for (int i = 0; i < a; i++) {
+            for (int i = 0; i < na; i++) {
                 System.out.print("*");
                 //Заполняет пробелами все кроме 1го столбца с "*"
-                if (i != 0 & i != a - 1) {
+                if (i != 0 & i != na - 1) {
                     //Центрирует строку
-                    for (int j = 0; j < b - 2; j++) {
-                        if (j == (b / 2) - (word.length() / 2)-1 & i == a / 2) {
+                    for (int j = 0; j < nb - 2; j++) {
+                        if (j == (nb / 2) - (word.length() / 2) - 1 & i == na / 2) {
                             System.out.print(word);
-
-                            j = (b / 2) + (word.length() / 2)-1;
-
-
+                            j = (nb / 2) + (word.length() / 2) - 1;
                         }
-
-
                         System.out.print(" ");
                     }
                     System.out.print("*");
-
                 }
                 //Рисует верхнюю и нижнюю границы
                 else {
-                    for (int j = 0; j < b - 1; j++) {
+                    for (int j = 0; j < nb - 1; j++) {
                         System.out.print("*");
                     }
                 }
-
-
                 System.out.println("");
             }
         }
     }
-
 }
 
