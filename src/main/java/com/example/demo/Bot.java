3e4e7e5f5e44f85fc5cc1f5a6617e1fe6654a724
AKochurova <kochurovaalena@mail.ru>
2020-12-30 21:28:22 +0700

    bot project

---
+++ src/main/java/com/example/demo/Bot.java
@@ -0,0 +1,98 @@
+package com.example.demo;
+import org.springframework.stereotype.Component;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class Bot extends TelegramLongPollingBot {
+
+    public void sendMsg(Message message, String text){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(text);
+        try{
+            setButton(sendMessage);
+            execute(sendMessage);
+
+        }catch (TelegramApiException e){
+            e.printStackTrace();
+        }
+    }
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        Model model = new Model();
+        ArrayList<Model> arr = new ArrayList<>();
+        for(int i=0; i<5; i++){
+            Model m = new Model();
+            arr.add(m);
+        }
+
+        if (message != null && message.hasText()){
+            switch (message.getText()){
+                case "/помощь":
+                    sendMsg(message, "Чем могу помочь?");
+                    break;
+                case "/settings":
+                    sendMsg(message, "What need to be set?");
+                    break;
+                default:
+                    try {
+
+                        //String res = Jobs.getJobs(message.getText(), model);
+                        //if (!res.isEmpty())
+                        for (int i=0; i<arr.size(); i++) {
+                            sendMsg(message, Jobs.getJobs(message.getText(), /*model*/ arr, i));
+                        }
+                        //else sendMsg(message, " Not found");
+
+
+
+                    }catch (IOException e){
+                        sendMsg(message, " Not found");
+                    }
+            }
+
+        }
+
+    }
+    public void setButton(SendMessage sendMessage){
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        List<KeyboardRow> keyboardRowList = new ArrayList<>();
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+        keyboardFirstRow.add(new KeyboardButton("/settings"));
+        keyboardFirstRow.add(new KeyboardButton("/help"));
+
+        keyboardRowList.add(keyboardFirstRow);
+        replyKeyboardMarkup.setKeyboard(keyboardRowList);
+    }
+
+
+    public String getBotUsername() {
+        return "MyTestBot";
+    }
+
+    public String getBotToken() {
+        return "1494861198:AAH8K7yIpRcohFyiLB_Ale_UAi_9U3l7RBE";
+    }
+
+
+}
