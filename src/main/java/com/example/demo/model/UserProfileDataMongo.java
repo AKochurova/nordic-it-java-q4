a35e549968c5bf02974df123dee6af200ffe8e25
AKochurova <kochurovaalena@mail.ru>
2021-02-10 11:12:01 +0700

    changed

--- src/main/java/com/example/demo/model/UserProfileDataMongo.java
+++ src/main/java/com/example/demo/model/UserProfileDataMongo.java
@@ -1,14 +1,12 @@
 package com.example.demo.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
 
-@Getter
-@Setter
+@Data
 @Document(collection = "userProfileData")
 public class UserProfileDataMongo implements Serializable {
     @Id
--- src/main/java/com/example/demo/service/UserProfileDataService.java
+++ src/main/java/com/example/demo/service/UserProfileDataService.java
@@ -2,15 +2,14 @@ package com.example.demo.service;
 
 import com.example.demo.model.UserProfileDataMongo;
 import com.example.demo.repository.UsersProfileMongoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserProfileDataService {
+    @Autowired
     private UsersProfileMongoRepository profileMongoRepository;
 
-    public UserProfileDataService(UsersProfileMongoRepository profileMongoRepository){
-        this.profileMongoRepository = profileMongoRepository;
-    }
 
     public void saveUserProfileData(UserProfileDataMongo profileDataId){
         profileMongoRepository.save(profileDataId);
